fbe7f99d22fa4340eb34843c2f606acf87d48730
Douglas Soares <douglassoares16.dev@gmail.com>
2022-06-27 20:14:59 -0300

    Created Button Component

---
+++ src/components/Form/Button/index.tsx
@@ -0,0 +1,14 @@
+import { TouchableOpacityProps } from "react-native";
+import { Container, Title } from "./styles";
+
+interface Props extends TouchableOpacityProps {
+  title: string;
+}
+
+export function Button({ title, ...rest }: Props) {
+  return (
+    <Container {...rest}>
+      <Title>{title}</Title>
+    </Container>
+  );
+}
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { Button } from "../../components/Form/Button";
 import { Input } from "../../components/Form/Input";
-import { Container, Form, Header, Title } from "./styles";
+import { Container, Form, Header, Title, Fields } from "./styles";
 
 export function Register() {
   return (
@@ -10,8 +11,12 @@ export function Register() {
       </Header>
 
       <Form>
-        <Input placeholder="Nome" />
-        <Input placeholder="Preço" />
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+        </Fields>
+
+        <Button title="Enviar" />
       </Form>
     </Container>
   );
