3992936f1f77a94d5efedebd479e45e1876cd68a
Douglas Soares <douglassoares16.dev@gmail.com>
2022-07-01 12:24:18 -0300

    feat: Created SignIn Screen

--- App.tsx
+++ App.tsx
@@ -10,8 +10,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import theme from './src/global/styles/theme';
-import { AppRoutes } from './src/routes/app.routes';
+// import { AppRoutes } from './src/routes/app.routes';
 import { StatusBar } from "react-native";
+import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -29,7 +30,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <NavigationContainer>
           <StatusBar barStyle="light-content" />
-          <AppRoutes />
+          <SignIn />
         </NavigationContainer>
       </ThemeProvider>
     </GestureHandlerRootView>
